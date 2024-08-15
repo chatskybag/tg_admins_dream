fa87f6ceb7dfd7026fa2b988d786da292833fb41
Вадим <chatskybag@mail.ru>
2024-08-16 02:40:26 +0300

    Убрал эксперименты, ждем новых

--- experiments.py
+++ experiments.py
@@ -1,5 +1,5 @@
 '''
-Эксперимент по обработке FloodWaitError, который возникает при чистке больших чатов
+Здесь будут эксперименты
 '''
 import time
 import asyncio
@@ -12,26 +12,10 @@ from time import sleep
 
 client = create_client()
 
-white_list = [1230480769, 1584816588, 428033786, 314211715, 137319637, 1158439141, 276585077, 6424014829, 7261927301]
 
-
-@client.on(events.NewMessage(outgoing=True, pattern='!flood'))
+@client.on(events.NewMessage(outgoing=True, pattern='!hello'))
 async def handler(event):
-    a = 0
-    request = "Some request"  # Замените на ваш запрос
-    seconds = 10  # Замените на время ожидания
-    for idx, user in enumerate(white_list):
-        try:
-            # await event.respond('!pong')
-            if idx == 5:
-                raise errors.FloodWaitError(request, seconds)
-            print("Число это: ", idx, user)
-        except errors.FloodWaitError as e:
-            print('Have to sleep', e.seconds, 'seconds')
-            time.sleep(e.seconds)
-            # просто добавить сюда действия как в блоке try
-            print("Число это: ", idx, user)
-            continue
+    await client.send_message('me', 'Hello to myself!')
 
 
 with client:
